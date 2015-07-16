5fd4d800872ed5d93ab536505c474af687a7707a
Lance Ball <lball@redhat.com>
2015-07-16 11:33:51 -0400

    Make it work as a module

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ A simple internet connection checker. I use this to log the status of my
 internet connection once per second, if I am having intermittent issues that
 are hard to diagnose. This is a good way to show Charter that the problem
 is theirs.
+
+    // Usage example. By default Check will log to the console, but not a file.
+    var Check = require('conchk'),
+      options = { silent-console: true, filename: 'output.log'},
+      checker = Check('google.com', 80, options);
+
+    setInterval(checker, 1000);
--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-var logger = initLog();
+// Usage example. By default Check will log to the console, but not a file.
+//
+// var Check = require('conchk'),
+//     checker = Check('google.com', 80, {silent-console: true, filename: 'conchk.log'});
+//
+// setInterval(checker, 1000);
 
-setInterval(checker('google.com', 80), 1000);
+module.exports = Check;
 
-function checker(host, port) {
+function Check(host, port, options) {
+  var logger = initLog(options);
   return function() {
-    checkConnection(host, port);
+    checkConnection(host, port, logger);
   };
-}
 
-function checkConnection(host, port) {
-  var net = require('net'),
-      opts = {
-        host: host,
-        port: port
-      };
+  function checkConnection(host, port, logger) {
+    var net = require('net'),
+        opts = {
+          host: host,
+          port: port
+        };
 
-  var conn = net.connect(opts, function(err, sock) {
-    if (err) {
-      logger.error('Connection error', err);
-    } else {
-      logger.info('Connected', opts);
-      if (sock) sock.close();
+    var conn = net.connect(opts, function(err, sock) {
+      if (err) {
+        logger.error('Connection error', err);
+      } else {
+        logger.info('Connected', opts);
+        if (sock) sock.close();
+      }
+    });
+
+    conn.on('error', function(e) {
+      logger.error('Connection error', e);
+    });
+  }
+
+  function initLog(options) {
+    var winston = require('winston');
+    winston.remove(winston.transports.Console);
+    winston.add(winston.transports.Console, { colorize:true, timestamp:true });
+
+    if (options) {
+      if (options.silent-console) {
+        winston.remove(winston.transports.Console);
+      }
+      if (options.filename) {
+        console.log("ADDING FILE " + options.filename);
+        winston.add(winston.transports.File, { filename: options.filename });
+      }
     }
-  });
-
-  conn.on('error', function(e) {
-    logger.error('Connection error', e);
-  });
-}
-
-function initLog() {
-  var winston = require('winston');
-  winston.remove(winston.transports.Console);
-  winston.add(winston.transports.Console, { colorize:true, timestamp:true });
-  winston.add(winston.transports.File, { filename: 'conchk.log' });
-  return winston;
+    return winston;
+  }
 }
